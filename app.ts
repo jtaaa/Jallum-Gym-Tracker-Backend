6c7427dd59a9b9563281f965e7de5c1c86293a86
jtaaa <joshuatallum@gmail.com>
2019-03-23 07:28:00 -0400

    Use session, passport and auth router

--- app.ts
+++ app.ts
@@ -3,12 +3,21 @@ import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
 import * as logger from 'morgan';
 
+import { passport, session } from './auth';
+
+import authRouter from './routes/auth';
+
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/auth', authRouter);
 
 // Error handler
 app.use((err, req, res, next) => {
