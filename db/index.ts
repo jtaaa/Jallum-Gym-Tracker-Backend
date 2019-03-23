038fe4ed373698c72dbe009cc92f420418933685
jtaaa <joshuatallum@gmail.com>
2019-03-23 07:25:56 -0400

    Add user model

--- db/index.ts
+++ db/index.ts
@@ -1,3 +1,4 @@
 export * from './models/session';
 export * from './models/set';
 export * from './models/exercise';
+export * from './models/user';
---
+++ db/models/user.ts
@@ -0,0 +1,50 @@
+import { Schema } from 'mongoose';
+import db from './../mongoose';
+
+const UserSchema = new Schema({
+  googleId: {
+    type: String,
+    index: true,
+    unique: true,
+    required: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: {
+      familyName: {
+        type: String,
+        required: true,
+      },
+      givenName: {
+        type: String,
+        required: true,
+      },
+    },
+    required: true,
+  },
+  gender: {
+    type: String,
+  },
+  photos: {
+    type: [ String ],
+    default: [],
+    required: true,
+  },
+  roles: {
+    type: [ String ],
+    default: [],
+    required: true,
+  },
+  dateAdded: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+const UserModel = db.model('user', UserSchema);
+
+export { UserModel };
