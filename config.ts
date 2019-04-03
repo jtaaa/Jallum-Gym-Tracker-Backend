b8706a8841eadb5f91cdcb288b471c52c3611e32
jtaaa <joshuatallum@gmail.com>
2019-04-03 17:25:46 -0400

    Add production HOMEPAGE_URL

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@ export default {
   GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
   GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
   EXPRESS_SESH_SECRET: process.env.EXPRESS_SESH_SECRET,
-  HOMEPAGE_URL: process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/' : '/',
+  HOMEPAGE_URL: process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/' : 'https://jallum.fitness',
   CORS_CONFIG: {
     origin: 'https://jallum.fitness',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
